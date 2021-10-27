98716ed776c5524f5db2c7abafd86ef46462fc95
Andrew Dalke <dalke@dalkescientific.com>
2021-10-27 13:45:16 +0200

    migrating to setup.cfp and pyproject.toml

--- setup.py
+++ setup.py
@@ -1,63 +1,6 @@
-# mmpdb - matched molecular pair database generation and analysis
-#
-# Copyright (c) 2015-2019, F. Hoffmann-La Roche Ltd.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above
-#      copyright notice, this list of conditions and the following
-#      disclaimer in the documentation and/or other materials provided
-#      with the distribution.
-#    * Neither the name of F. Hoffmann-La Roche Ltd. nor the names of
-#      its contributors may be used to endorse or promote products
-#      derived from this software without specific prior written
-#      permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
+#!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
-with open("README.md") as f:
-    long_description = f.read()
-
-setup(
-    name="mmpdb",
-    version="2.3-dev1",
-    description="A package to identify matched molecular pairs and use them to predict property changes",
-    author="Andrew Dalke",
-    author_email="dalke@dalkescientific.com",
-    maintainer="Christian Kramer",
-    maintainer_email="rdkit-discuss@lists.sourceforge.net",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rdkit/mmpdb",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Chemistry",
-        ],
-    keywords="matched molecular pair, MMP, cheminformatics",
-    
-    packages=["mmpdblib", "mmpdblib.playhouse"],
-    package_data={"mmpdblib": ["schema.sql", "create_index.sql", "drop_index.sql"]},
-    scripts=["mmpdb"]
-    )
+if __name__ == "__main__":
+    setuptools.setup()
